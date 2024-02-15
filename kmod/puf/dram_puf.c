6eda85ae5dd2d65d4ffd6254af0fa59ccf726559
despire <matus.mrekaj@icloud.com>
2024-02-15 11:10:22 +0100

    update kmod

--- kmod/puf/dram_puf.c
+++ kmod/puf/dram_puf.c
@@ -56,61 +56,13 @@ static struct delayed_work work_temp_poll;
 #include "device.c"
 
 /////////////////////////////////////
+//          BBB memory access.     //
+////////////////////////////////////
+#include "memory.c"
+
+/////////////////////////////////////
 //          PUF related code.      //
 ////////////////////////////////////
-int hw_reg_r32(uint32_t addr, uint32_t *out) {
-    uint32_t *virt_addr = (uint32_t *) ioremap(addr, sizeof(uint32_t));
-    if (!virt_addr) {
-        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
-        return -1;
-    }
-
-    *out = *virt_addr;
-    iounmap((void *) virt_addr);
-
-    return 0;
-}
-
-int hw_reg_w32(uint32_t addr, uint32_t val) {
-    uint32_t *virt_addr = (uint32_t *) ioremap(addr, sizeof(uint32_t));
-    if (!virt_addr) {
-        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
-        return -1;
-    }
-
-    *virt_addr = val;
-    iounmap((void *) virt_addr);
-
-    return 0;
-}
-
-#pragma GCC push_options
-#pragma GCC optimize ("O2")
-int phys_r32(uint32_t addr, uint32_t *out) {
-    uint32_t *virt_addr = (uint32_t *) phys_to_virt(addr);
-    if (!virt_addr) {
-        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
-        return -1;
-    }
-
-    *out = *virt_addr;
-
-    return 0;
-}
-
-int phys_w32(uint32_t addr, uint32_t value) {
-    uint32_t *virt_addr = (uint32_t *) phys_to_virt(addr);
-    if (!virt_addr) {
-        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
-        return -1;
-    }
-
-    *virt_addr = value;
-
-    return 0;
-}
-#pragma GCC pop_options
-
 void temp_polling(struct work_struct *work) {
     uint32_t tctrl, temp;
     struct delayed_work *dwork;
---
+++ kmod/puf/memory.c
@@ -0,0 +1,52 @@
+#pragma GCC push_options
+#pragma GCC optimize ("O2")
+int hw_reg_r32(uint32_t addr, uint32_t *out) {
+    uint32_t *virt_addr = (uint32_t *) ioremap(addr, sizeof(uint32_t));
+    if (!virt_addr) {
+        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
+        return -1;
+    }
+
+    *out = *virt_addr;
+    iounmap((void *) virt_addr);
+
+    return 0;
+}
+
+int hw_reg_w32(uint32_t addr, uint32_t val) {
+    uint32_t *virt_addr = (uint32_t *) ioremap(addr, sizeof(uint32_t));
+    if (!virt_addr) {
+        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
+        return -1;
+    }
+
+    *virt_addr = val;
+    iounmap((void *) virt_addr);
+
+    return 0;
+}
+
+int phys_r32(uint32_t addr, uint32_t *out) {
+    uint32_t *virt_addr = (uint32_t *) phys_to_virt(addr);
+    if (!virt_addr) {
+        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
+        return -1;
+    }
+
+    *out = *virt_addr;
+
+    return 0;
+}
+
+int phys_w32(uint32_t addr, uint32_t value) {
+    uint32_t *virt_addr = (uint32_t *) phys_to_virt(addr);
+    if (!virt_addr) {
+        printk(KERN_ERR "failed to read physical address=0x%08x\n", addr);
+        return -1;
+    }
+
+    *virt_addr = value;
+
+    return 0;
+}
+#pragma GCC pop_options
