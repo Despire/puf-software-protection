01b783d66d86f131f720bf74795ba92a1b0342ed
despire <matus.mrekaj@icloud.com>
2024-04-11 19:21:01 +0200

    adjust enrollments

--- enrollments/scripts/enroll.sh
+++ enrollments/scripts/enroll.sh
@@ -16,12 +16,12 @@ for num in 50; do # "num_of_measurements (used in the JSON config, i.e. this wou
         iter=$((i + 0))
         echo "Iteration $iter"
 
-        ssh -q root@beaglebone.local insmod /home/debian/puf/dram_puf.ko puf_start_address=${puf_start_address} puf_block_1_size=${puf_size}
+        ssh -q root@beaglebone.local insmod /home/debian/puf/dram_puf.ko puf_block_1=${puf_start_address} puf_block_1_size=${puf_size}
         echo "recording dram cells at $sleep_seconds secs of decay"
         sleep ${sleep_seconds}
         ssh -q root@beaglebone.local rmmod dram_puf
 
-        ssh -q root@beaglebone.local cat /dev/puf_start_address >  "${folder_name}/BBB_${iter}_${num}sec"
+        ssh -q root@beaglebone.local cat /dev/puf_block_1 >  "${folder_name}/BBB_${iter}_${num}sec"
         echo "Done with Iteration $iter!"
         echo "wait 5 mins before next iteration"
         sleep 300
